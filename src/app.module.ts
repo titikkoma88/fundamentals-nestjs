49aa42aa2938289ef6a29b8b94458cc1b8564e9f
titikkoma88 <jie.koma88@gmail.com>
2026-01-27 01:26:41 +0700

    Instalation and configuration

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ProductService } from './product/product.service';
+import { ProductController } from './product/product.controller';
+import { ProductModule } from './product/product.module';
+
+@Module({
+  imports: [ProductModule],
+  controllers: [AppController, ProductController],
+  providers: [AppService, ProductService],
+})
+export class AppModule {}
