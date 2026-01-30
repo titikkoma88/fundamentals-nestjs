11028e0064eac5d2e567b3539e8f802a61935942
titikkoma88 <jie.koma88@gmail.com>
2026-01-31 01:59:51 +0700

    add database and typeorm connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,30 @@ import { AppService } from './app.service';
 import { ProductService } from './product/product.service';
 import { ProductController } from './product/product.controller';
 import { ProductModule } from './product/product.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ProductModule],
+  imports: [
+    ProductModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) :{} => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        synchronize: true,
+        autoLoadEntities: true,
+      })
+    })
+  ],
   controllers: [AppController, ProductController],
   providers: [AppService, ProductService],
 })
