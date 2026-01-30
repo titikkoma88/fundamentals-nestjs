56f490c9d9420de4d1761cb7e9d0663115c4851d
titikkoma88 <jie.koma88@gmail.com>
2026-01-31 01:25:56 +0700

    input validation and dto

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { ProductService } from './product.service';
+import { CreateProductDto } from './dto/create-product.dto';
 
 @Controller('product')
 export class ProductController {
@@ -8,5 +9,10 @@ export class ProductController {
     @Get('list')
     getProductList(): string[] {
         return this.productService.getProductList();
+    }
+
+    @Post()
+    createProduct(@Body() data: CreateProductDto): object {
+        return this.productService.createProduct(data);
     }
 }
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
 
 @Injectable()
 export class ProductService {
     public getProductList(): string[] {
         return ['Product 1', 'Product 2', 'Product 3']
+    }
+
+    public createProduct(data: CreateProductDto): object {
+        return {
+            name: data.name,
+            price: data.price,
+            description: data.description,
+            stock: data.stock,
+        };
     }
 }
